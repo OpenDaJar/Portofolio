a7f73d989e4e6a74b7fa9f0568534bb38ef4a1ee
Ladas Michail <47851156+OpenDaJar@users.noreply.github.com>
2023-06-27 12:32:54 +0300

    welcome section add

--- myscripts.js
+++ myscripts.js
@@ -2,23 +2,35 @@
 console.log("Hello from JS");
 // sidebar.test()
 
-
 //Welcome text transform
 const welcomeTransform = () => {
-  console.log("Welcome");
+  const welcomeTextElement = document.getElementById("welcome__original");
+  const changedTextElement = document.getElementById("welcome__changed");
+  const textToChange = welcomeTextElement.innerText.split("");
+  let changedText = [];
+  textToChange.forEach((letter, i) => {
+    setTimeout(() => {
+      // letterElement.innerText = letter
+      changedText.push(letter);
+      changedTextElement.innerText = changedText.join("");
+
+      textToChange.shift();
+      welcomeTextElement.innerText = textToChange.join("");
+    }, i * 350);
+  });
 };
 
 // Expand/collapse btn for sidebar
 const expandButton = () => {
   const sidebarElement = document.getElementById("sidebar");
-  const mainElement = document.getElementById("main");
+  const mainElement =  document.getElementById("main")
   const sidebarStyles = window.getComputedStyle(sidebarElement);
   const sidebarVisibility = sidebarStyles.visibility;
-  const arrowExpand = document.getElementById('arrow_expand')
+  const arrowExpand = document.getElementById("arrow_expand");
 
   if (sidebarVisibility === "visible") {
-     //Arrow expand rotate
-    arrowExpand.style.rotate = "0deg"
+    //Arrow expand rotate
+    arrowExpand.style.rotate = "0deg";
     // sidebar
     sidebarElement.style.visibility = "collapse";
     sidebar.style.width = "0%";
@@ -26,7 +38,7 @@ const expandButton = () => {
     mainElement.style.width = "100%";
   } else if (sidebarVisibility === "collapse") {
     //Arrow expand rotate
-    arrowExpand.style.rotate = "180deg"
+    arrowExpand.style.rotate = "180deg";
     // sidebar
     sidebarElement.style.visibility = "visible";
     sidebar.style.width = "20%";
